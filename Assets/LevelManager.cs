8bdd9373dc23f1d27ee1e88f2480b7972e6c14e7
A333 <arz.giyan@gmail.com>
2023-04-29 09:56:53 +0400

    adasdasd

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -18,6 +18,6 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        ship.RB.AddForce(-ship.transform.position * PullToCenter * Time.fixedDeltaTime);
+        ship.RB.AddForce(PullToCenter * Time.fixedDeltaTime * -ship.transform.position);
     }
 }
