dbfb1a4045350caa75c2997acc2ed3b298a0b99c
Ruslan Kibets <ruslan.kibets@gmail.com>
2023-04-29 12:43:02 -0700

    Landing Pad tween landing

--- Assets/Scripts/Consts.cs
+++ Assets/Scripts/Consts.cs
@@ -1,4 +1,8 @@
 ﻿public static class Consts
 {
     public static float PlanetDiameter = 5.0f; // average
+
+
+    public static float LandingPadLandingTime = 1.13f;
+    public static float LandingPadLaunchForce = 20f;
 }
--- Assets/Scripts/LandingPad.cs
+++ Assets/Scripts/LandingPad.cs
@@ -1,13 +1,58 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class LandingPad : MonoBehaviour
 {
+    private Ship _ship;
+    private bool _shipLanded;
+    private bool _visited;
+    
     private void Awake()
     {
         Objects.Instance.AddLandingPad(this);
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Deploy();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_visited) return;
+        
+        if (other.CompareTag("Ship"))
+        {
+            var ship = other.GetComponentInParent<Ship>();
+
+            ship.RB.isKinematic = true;
+
+            _ship = ship;
+            _visited = true;
+
+            ship.transform.DOMove(transform.position, Consts.LandingPadLandingTime).OnComplete(() =>
+            {
+                _shipLanded = true;
+            });
+
+            ship.transform.DORotateQuaternion(transform.rotation, Consts.LandingPadLandingTime * 0.5f);
+        }
+    }
+    
+    public void Deploy()
+    {
+        if (_ship == null) return;
+        if (!_shipLanded) return;
+
+        _ship.RB.isKinematic = false;
+        _ship.RB.AddForce(_ship.transform.forward * Consts.LandingPadLaunchForce, ForceMode.VelocityChange);
+        _ship = null;
+        _shipLanded = false;
     }
 
     private void OnDestroy()
