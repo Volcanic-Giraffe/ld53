911fb8434e31bd7c4d5596ea8e4fb7312aad88a4
Ruslan Kibets <ruslan.kibets@gmail.com>
2023-04-29 20:15:29 -0700

    Thrust sound

--- Assets/Scripts/Effects/EngineFx.cs
+++ Assets/Scripts/Effects/EngineFx.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class EngineFx : MonoBehaviour
 {
     [SerializeField] private ParticleSystem mainPs;
+    [SerializeField] private AudioSource asThrust;
+
+    private void Awake()
+    {
+        asThrust.Play();
+    }
 
     public void Stop()
     {
         mainPs.Stop();
+        
+        asThrust.DOKill();
+        asThrust.DOFade(0.0f, 0.13f);
     }
 
     public void Start()
     {
         mainPs.Play();
+
+        asThrust.DOKill();
+        asThrust.DOFade(0.5f, 0.23f);
     }
 }
