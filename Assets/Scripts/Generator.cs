bf273ca387f9e1b834a093a5d26b615d691c8584
A333 <arz.giyan@gmail.com>
2023-05-01 00:16:27 +0400

    VAPORWAVE

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -9,13 +9,14 @@ public class Generator : Singleton<Generator>
 {
     public float Radius;
     public int Count;
+    [SerializeField] private GameObject ringPrefab;
 
     public int FuelPickupCount;
-    
+
     public event Action OnGenerationDone;
 
     private List<Planet> _planets = new();
-    
+
     private void Start()
     {
         StartCoroutine(GenerateCR());
@@ -42,12 +43,12 @@ public class Generator : Singleton<Generator>
 
         for (int i = 0; i < FuelPickupCount; i++)
         {
-            var planet = _planets.Where(p=> p.LandingPad == null).PickRandom();
-            
+            var planet = _planets.Where(p => p.LandingPad == null).PickRandom();
+
             var pickup = Prefabs.Instance.Produce<FuelPickup>();
             pickup.transform.position = planet.transform.position + Vector3.up * planet.Diameter * 0.6f;
         }
-        
+
         yield return new WaitForEndOfFrame();
 
         OnGenerationDone?.Invoke();
@@ -66,11 +67,29 @@ public class Generator : Singleton<Generator>
             while (_planets.Any(p => Vector3.Distance(planet.transform.position, p.transform.position) < (Radius / 5f)) && attempts > 0)
             {
                 attempts -= 1;
-                
+
                 planet.transform.position = Random.insideUnitSphere * Radius;
             }
-            
+            if (Random.value > 0.9f)
+            {
+                var rotation = Random.rotation;
+                CreateRing(planet, Random.Range(1.2f, 1.4f), rotation);
+                if(Random.value > 0.75f)
+                {
+                    CreateRing(planet, Random.Range(1.6f, 2f), rotation);
+                }
+            }
             _planets.Add(planet);
         }
+    }
+
+    private void CreateRing(Planet planet, float size, Quaternion rotation)
+    {
+        var ring = Instantiate(ringPrefab);
+        ring.GetComponentInChildren<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 0.5f, 1f, 0.7f, 1f);
+        ring.transform.SetParent(planet.transform);
+        ring.transform.localPosition = Vector3.zero;
+        ring.transform.localScale = Vector3.one * planet.Diameter * size;
+        ring.transform.localRotation = rotation;
     }
 }
