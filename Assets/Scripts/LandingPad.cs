4877adea9734ea422c53d67f511235de866d893c
Ruslan Kibets <ruslan.kibets@gmail.com>
2023-04-29 22:20:11 -0700

    LMB to deploy

--- Assets/Scripts/LandingPad.cs
+++ Assets/Scripts/LandingPad.cs
@@ -23,7 +23,7 @@ public class LandingPad : MonoBehaviour
     
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             Deploy();
         }
