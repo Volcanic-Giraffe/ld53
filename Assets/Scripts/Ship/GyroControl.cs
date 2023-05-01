fbcf1fccd60f08acb3e04bfa173d2cee19328a57
A333 <arz.giyan@gmail.com>
2023-05-01 16:56:30 +0400

    Help me stepbro

--- Assets/Scripts/Ship/GyroControl.cs
+++ Assets/Scripts/Ship/GyroControl.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class GyroControl : MonoBehaviour
 {
+
     private Rigidbody rb;
     private Ship _ship;
 
@@ -15,9 +16,13 @@ public class GyroControl : MonoBehaviour
 
     private CameraController _cam;
 
+    private float _stuckTimer;
+    private float _initialGyroPower;
+
     // Start is called before the first frame update
     void Awake()
     {
+        _initialGyroPower = GyroPower;
         rb = GetComponent<Rigidbody>();
         _ship = GetComponent<Ship>();
         _cam = FindObjectOfType<CameraController>();
@@ -27,6 +32,18 @@ public class GyroControl : MonoBehaviour
     {
         if (_ship.Standby) return;
         _rotation = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 0.5f, LayerMask.GetMask("Planet")))
+        {
+            _stuckTimer += Time.deltaTime;
+            if (_stuckTimer > 3f) GyroPower = _initialGyroPower * 10f;
+        }
+        else
+        {
+            _stuckTimer = 0;
+            GyroPower = _initialGyroPower;
+        }
     }
 
     // Update is called once per frame
